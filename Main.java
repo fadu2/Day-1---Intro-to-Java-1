378a2d911f12a1b6bf846568606a5c0134e27886
FrancisOhemengA <93487638+fadu2@users.noreply.github.com>
2021-11-02 19:53:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,40 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    // Our first day
+
+    //#1 String* - e.g. "hello", "Chase", "3.14"
+    //#2 integer* - e.g. 1, 23, 100, 1000
+    //#3 boolean* - e.g. true, false
+    // #4 float - e.g. 2.5, 1.444
+    // #5 char* - e.g. 'E', 'e', '2'
+    // #6 double* - e.g. 3.14, 100.0
+
+    int age; // variable declaration
+    age = 16; // variable assignment or definition
+
+    int age2 = 16;
+
+    double price = 499;
+    int price2 = 10;
+
+    String name = "John";
+    String school = "Regis High School";
+
+    boolean isHot = true;
+    boolean isCold = false;
+
+    boolean isOld = true;
+    System.out.println("hello");
+    System.out.println(7 + 8);
+    System.out.println("hello" + 7 + 8);
+    System.out.println("hello" + (7 + 8));
+
+
+
+
+
+
+
+  }
+}
